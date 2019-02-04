d574d73aa0491bd4208eae273d41e73235c96ab9
Sakti R <reysaputrasakti@gmail.com>
2019-02-04 14:23:17 +0100

    1st Commit Repo

---
+++ Animation_Unity_2018_2/Assets/scripts/NewJump.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewJump : MonoBehaviour {
+
+    Animator anim;
+    bool move = false;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.J)) {
+
+            move = !move;
+            anim.SetBool("bool1", move);
+        }
+	}
+}
